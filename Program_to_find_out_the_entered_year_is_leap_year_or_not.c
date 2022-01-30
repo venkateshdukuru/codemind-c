606f9149ac5ad30c126f59a34e6bdb4273792ec1
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-01-30 12:07:06 +0530

    #include <stdio.h> int main() {     int y;     scanf("%d",&y);
        if(y % 4 == 0)
        {
        	//Nested if else
            if( y % 100 == 0)
            {
                if ( y % 400 == 0)
                   printf("True");
                else
                    printf("False");
            }
            else
                printf("True");
        }
        else
            printf("False");
    
        return 0;
    }

---
+++ Program_to_find_out_the_entered_year_is_leap_year_or_not.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int y;
+    scanf("%d",&y);
+
+    if(y % 4 == 0)
+    {
+    	//Nested if else
+        if( y % 100 == 0)
+        {
+            if ( y % 400 == 0)
+               printf("True");
+            else
+                printf("False");
+        }
+        else
+            printf("True");
+    }
+    else
+        printf("False");
+
+    return 0;
+}
