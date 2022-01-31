95279ec5062a145a8ef788c9e9d9eab11eb99a8e
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-01-31 22:34:01 +0530

    #include<stdio.h> int main() {     int n;     printf("");     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         for(int j=1;j<=n;j++)         {         if(j==1||i==j||i==n )         {             printf("*");         }         else         printf(" ");         }         printf(" ");     }     return 0; }

---
+++ print_the_following_pattern.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()  
+{  
+    int n;  
+    printf("");  
+    scanf("%d",&n);  
+    for(int i=1;i<=n;i++)  
+    {  
+        for(int j=1;j<=n;j++)  
+        {  
+        if(j==1||i==j||i==n )  
+        {  
+            printf("*");  
+        }  
+        else  
+        printf(" ");  
+        }  
+        printf("
+");  
+    }  
+    return 0;  
+}
