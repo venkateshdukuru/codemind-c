702088aa7cf1f14bd41d4aa483e311a5c79e7328
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-01-31 07:01:53 +0530

    #include<stdio.h> #include<math.h> int main() {     int n;     scanf("%d",&n);     int n1=n,c=0;     while(n1!=0)     {         n1/=10;         c++;     }     n1=n;     int s=0;     while(n1!=0)     {         int rem=n1%10;         s+=pow(rem,c);         n1/=10;         c--;     }
        if(s==n)
        printf("True");
        else
        printf("False");
    }

---
+++ Disarium_number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int n1=n,c=0;
+    while(n1!=0)
+    {
+        n1/=10;
+        c++;
+    }
+    n1=n;
+    int s=0;
+    while(n1!=0)
+    {
+        int rem=n1%10;
+        s+=pow(rem,c);
+        n1/=10;
+        c--;
+    }
+    
+    
+    if(s==n)
+    printf("True");
+    else
+    printf("False");
+}
