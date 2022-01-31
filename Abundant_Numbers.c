0c1eb55d1070172ea4a3ac02a702221b78c6e345
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-01-31 07:13:57 +0530

    # include <stdio.h> # include <string.h> # include <stdbool.h> # include <math.h> int getSum(int n) {     int sum = 0;     for (int i=1; i<=sqrt(n); i++)     {         if (n%i==0)         {             if (n/i == i)                 sum = sum + i;             else             {                 sum = sum + i;                 sum = sum + (n / i);             }         }     }     sum = sum - n;     return sum; } bool checkAbundant(int n) {     return getSum(n) > n; } int main() { int n; scanf("%d",&n); checkAbundant(n)? printf("True ") : printf("False ");     return 0; }

---
+++ Abundant_Numbers.c
@@ -0,0 +1,36 @@
+# include <stdio.h>  
+# include <string.h>  
+# include <stdbool.h>  
+# include <math.h>  
+int getSum(int n)  
+{  
+    int sum = 0;  
+    for (int i=1; i<=sqrt(n); i++)  
+    {  
+        if (n%i==0)  
+        {  
+            if (n/i == i)  
+                sum = sum + i;  
+            else   
+            {  
+                sum = sum + i;  
+                sum = sum + (n / i);  
+            }  
+        }  
+    }  
+    sum = sum - n;  
+    return sum;  
+}  
+bool checkAbundant(int n)  
+{  
+    return getSum(n) > n;  
+}  
+int main()  
+{  
+int n;  
+scanf("%d",&n);  
+checkAbundant(n)? printf("True
+") : printf("False
+");  
+    return 0;  
+}
