0f04fa43109901ab870e0e88b551947251347ce0
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-01-31 22:37:58 +0530

    #include<stdio.h> int main() {     int n;     printf("");     scanf("%d",&n);     int m=n;     for(int i=1;i<=n;i++)     {        for(int j=1;j<=n;j++)        {            if(i==j||j==m)            printf("x");            else            printf("0");        }        printf(" ");        m--;     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -4,17 +4,19 @@ int main()
     int n;
     printf("");
     scanf("%d",&n);
+    int m=n;
     for(int i=1;i<=n;i++)
     {
-        for(int j=1;j<=n;j++)
-        {
-            if(i==j||j==n||j==1)
-            printf("* ");
-            else
-            printf("  ");
-        }
-        printf("
+       for(int j=1;j<=n;j++)
+       {
+           if(i==j||j==m)
+           printf("x");
+           else
+           printf("0");
+       }
+       printf("
 ");
+       m--;
     }
     return 0;
 }
