9bc3f2b99fcb6cbefb1f367e1c03e8f25a282705
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-05-29 08:08:22 +0530

    #include<stdio.h> #include<math.h> int main() {     int a,b,i,j,x=0,c=0;     scanf("%d",&a);     scanf("%d",&b);     for(i=a;i<=b;i++)     {         if(i==1)         {             continue;         }         x=0;         for(j=2;j<=sqrt(i);j++)         {             if(i%j==0)             {                 x+=1;                 break;             }         }         if(x==0)         {             c+=1;         }     }
        printf("%d",c);
    }

--- Primes_in_range.c
+++ Primes_in_range.c
@@ -1,50 +1,30 @@
-#include<stdio.h>  
-#include<math.h>  
-  
-int isPrime(int num)  
-{  
-    int inum = sqrt(num), prime = 1, count;  
-  
-    for(count = 2; count <= inum; count++)  
-    {  
-         if(num % count == 0)  
-         {  
-            prime = 0;  
-            break;  
-         }  
-    }  
-  
-    return(prime);  
-}  
-  
-int main()  
-{  
-    int start, end, temp, num, slno = 0;  
-  
-    scanf("%d%d", &start, &end);  
-  
-
-    if(start > end)  
-    {  
-        temp  = start;  
-        start = end;  
-        end   = temp;  
-    }  
-  
-
-    for(num = start; num <= end; num++)  
-    {  
-        if(num == 1)  
-        {  
-            continue;  
-        }  
-  
-        if( isPrime(num) )  
-        {  
-            slno++;  
-         }  
-    }  
-  
-    printf("%d",slno);  
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,i,j,x=0,c=0;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    for(i=a;i<=b;i++)
+    {
+        if(i==1)
+        {
+            continue;
+        }
+        x=0;
+        for(j=2;j<=sqrt(i);j++)
+        {
+            if(i%j==0)
+            {
+                x+=1;
+                break;
+            }
+        }
+        if(x==0)
+        {
+            c+=1;
+        }
+    }
+    
+    printf("%d",c);
 }
-  
