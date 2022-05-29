fc29f44a105e26b948c324295892ebb7387ededd
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-05-29 08:26:41 +0530

    #include<stdio.h> int main() {     int n,a[100],i,max=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     max=a[0];     for(i=0;i<n;i++)     {         if(max<a[i])         {             max=a[i];         }     }     printf("%d",max); }

---
+++ maximum_of_an_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,max=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    max=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(max<a[i])
+        {
+            max=a[i];
+        }
+    }
+    printf("%d",max);
+}
