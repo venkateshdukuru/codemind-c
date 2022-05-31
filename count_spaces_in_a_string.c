dad6285662e94aa7868043c1dcfe4e016a6e5058
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-05-31 19:50:01 +0530

    #include<stdio.h> #include<stdlib.h> #include<string.h> int main() {     char str[1000];     int i,c=0,len;     scanf("%[^ ]s",str);     len=strlen(str);     for(i=0;i<len;i++)     {         if(str[i]==' ')         {             c++;         }     }     printf("%d",c); }

---
+++ count_spaces_in_a_string.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    char str[1000];
+    int i,c=0,len;
+    scanf("%[^
+]s",str);
+    len=strlen(str);
+    for(i=0;i<len;i++)
+    {
+        if(str[i]==' ')
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
