f4a3ef5b6e7b273f1ffc75db1fb1a386cdde1f71
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-05-31 19:30:26 +0530

    #include<stdio.h> int main() {     int n,k,arr[100],i,c=0;     scanf("%d%d",&n,&k);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%k!=0)         {             c++;         }     }     printf("%d",c); }

---
+++ Count_elements_not_divisible_by_K.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,arr[100],i,c=0;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%k!=0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
