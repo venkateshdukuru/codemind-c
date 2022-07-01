0aa556997e302470da725af278820ed37a4148ce
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-07-01 22:15:35 +0530

    #include<stdio.h> int main() {     int n,arr[100],i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             printf("%d ",arr[i]);         }     }     for(i=0;i<n;i++)     {         if(arr[i]%2)         {             printf("%d ",arr[i]);         }     } }

--- even_elements_followed_by_odd.c
+++ even_elements_followed_by_odd.c
@@ -1,24 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int n,a[100],i;
+    int n,arr[100],i;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d",&arr[i]);
     }
     for(i=0;i<n;i++)
     {
-        if(a[i]%2==0)
+        if(arr[i]%2==0)
         {
-            printf("%d ",a[i]);
+            printf("%d ",arr[i]);
         }
     }
     for(i=0;i<n;i++)
     {
-        if(a[i]%2!=0)
+        if(arr[i]%2)
         {
-            printf("%d ",a[i]);
+            printf("%d ",arr[i]);
         }
     }
 }
