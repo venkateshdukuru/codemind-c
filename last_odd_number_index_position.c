59cc7fde33df863eb3721826d10aa7073d176016
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-07-01 22:35:49 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,ind;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2)         {             ind=i;         }     }     printf("%d",ind); }

--- last_odd_number_index_position.c
+++ last_odd_number_index_position.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,a[100],i,ind;
+    int n,arr[100],i,ind;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d",&arr[i]);
     }
     for(i=0;i<n;i++)
     {
-        if(a[i]%2!=0)
+        if(arr[i]%2)
         {
             ind=i;
         }
