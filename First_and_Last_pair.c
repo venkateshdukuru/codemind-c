0450c8bebb75052197338d8f85bcd79a14e89e7e
Venkatesh <98198945+venkateshdukuru@users.noreply.github.com>
2022-07-01 21:55:42 +0530

    #include<stdio.h> int main() {     int n,i,a[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         //printf("%d ",a[i]);     }     if(n%2==0)     {         for(i=0;i<n/2;i++)         {             printf("%d %d ",a[i],a[n-i-1]);         }
        }
        else
        {
            for(i=0;i<n/2;i++)
            {
                printf("%d %d ",a[i],a[n-i-1]);
            }
            printf("%d 0",a[(n/2)]);
            
        }
    }

--- First_and_Last_pair.c
+++ First_and_Last_pair.c
@@ -1,19 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int n,a[100],i;
+    int n,i,a[100];
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
-    if(n%2)
+    for(i=0;i<n;i++)
+    {
+        //printf("%d ",a[i]);
+    }
+    if(n%2==0)
     {
         for(i=0;i<n/2;i++)
         {
             printf("%d %d ",a[i],a[n-i-1]);
         }
-        printf("%d 0",a[n/2]);
+        
     }
     else
     {
@@ -21,5 +25,7 @@ int main()
         {
             printf("%d %d ",a[i],a[n-i-1]);
         }
+        printf("%d 0",a[(n/2)]);
+        
     }
 }
